0a632d91b003b07b1472ae560bf04f5de3827e90
Abhishek Choubey <choubeyabhishek53@gmail.com>
2023-08-10 07:53:24 +0530

    first commit

---
+++ src/app/shared/services/products.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { Product } from '../type/product.interface';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  currency: string = "INR";
+
+  constructor(private _httpClient: HttpClient, private _toastr: ToastrService) { }
+
+  private allProducts(): Observable<Product[]> {
+    return this._httpClient.get<Product[]>(environment.BASE_API_PATH + "ProductMaster/GetProductList");
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this.allProducts();
+  }
+
+}
